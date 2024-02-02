d7d9e8fcceadb62aa4d0a4197d162f3a949eb356
Lexh19 <iqbaalhzd@gmail.com>
2024-02-02 18:10:56 +0700

    latihan react

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './App/Screens/LoginScreen/Login';
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
+
+const tokenCache = {
+  async getToken(key) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ClerkProvider 
+    tokenCache={tokenCache}
+    publishableKey='pk_test_ZmxlZXQtZHJ1bS02Ni5jbGVyay5hY2NvdW50cy5kZXYk'>
+      <View style={styles.container}>
+
+      {/*sign in component*/}
+        <SignedIn>
+          <Text>You are Signed in</Text>
+        </SignedIn>
+        {/*signout component*/}
+        <SignedOut>
+          <Login/>
+        </SignedOut>
+        <StatusBar style="auto" />
+      </View>
+    </ClerkProvider>
   );
 }
 
@@ -14,7 +46,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 20
   },
 });
---
+++ App/Screens/LoginScreen/styles.jsx
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+import Color from '../Utils/Color';
+
+export const styles = StyleSheet.create({
+loginImage: {
+width: 230,
+height: 450,
+marginTop: 70,
+borderWidth: 4,
+borderColor: Color.BLACK,
+borderRadius: 15
+},
+subContainer: {
+width: '100',
+backgroundColor: Color.PRIMARY,
+height: '70',
+marginTop: -20,
+borderTopLeftRadius: 30,
+borderTopRightRadius: 30
+}
+});
